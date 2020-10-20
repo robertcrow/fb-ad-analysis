693d7a328c90e28b952d5ce5332f52dddbf07970
robertcrow <r.crow@quickersim.com>
2020-10-20 19:01:02 +0200

    move function files to designated folders

--- fb_ad_analysis/api/__init__.py
+++
@@ -1,4 +0,0 @@
-from .connectToAPI import connectToAPI
-from .queryAPI import queryAPI
-from .fetchAds import fetchAds
-from .cleanData import cleanData
---
+++ fb_ad_analysis/fetch/__init__.py
@@ -0,0 +1,4 @@
+from .connect_to_api import connect_to_api
+from .query_api import query_api
+from .fetch_ads import fetch_ads
+from .clean_data import clean_data
